cf1fc131ce019fb982abb70a45e2c64b7c8dfaf7
Anubhav Bahadur Mathur <42437396+AnubhavBM@users.noreply.github.com>
2024-09-16 20:49:07 +0530

    Delete AssertionsTest.java

--- AssertionsTest.java
+++
@@ -1,8 +0,0 @@
-public class AssertionsTest {
-    public static void main(String[] args)
-    {
-        int age = 2;
-        assert age >= 18 : "Cannot Vote";
-        System.out.println("The voter's age is " + age);
-    }
-}
