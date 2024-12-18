c024c33ad3203c616b1b35f8019099a7e03dfd8d
AnubhavBM <anubhav.bahadur1106@gmail.com>
2024-12-19 00:18:42 +0530

    Operations on an Arraynumber 2460

---
+++ OperationsArray.java
@@ -0,0 +1,25 @@
+public class OperationsArray {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,2,1,1,0};
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+        }
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        while(j<nums.length){
+            nums[j]=0;
+            j++;
+        }
+        for(int val:nums){
+            System.out.print(val+" ");
+        }
+    }
+}
